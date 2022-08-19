740247660fdfb6480335c9e6b1e3273cac12894b
AliQasimzade <qasimzadeali4@gmail.com>
2022-08-19 12:56:25 +0400

    js calculator

--- main.js
+++ main.js
@@ -1,112 +1,49 @@
-const input = document.querySelector(".input");
-const result = document.querySelector(".result");
-const deleteBtn = document.querySelector(".delete");
-const keys = document.querySelectorAll(".bottom span");
-
-let operation = "";
-let answer;
-let decimalAdded = false;
-
-const operators = ["+", "-", "x", "÷"];
-
-const handleKeyPress = (e)  => {
-  const key = e.target.dataset.key;
-  const lastChar = operation[operation.length - 1];
-
-  if (key === "=") {
-    return;
-  }
-
-  if (key === "." && decimalAdded) {
-    return;
-  }
-
-  if (operators.indexOf(key) !== -1) {
-    decimalAdded = false;
-  }
-
-  if (operation.length === 0 && key === "-") {
-    operation += key;
-    input.innerHTML = operation;
-    return;
-  }
-
-  if (operation.length === 0 && operators.indexOf(key) !== -1) {
-    input.innerHTML = operation;
-    return;
-  }
-
-  if (operators.indexOf(lastChar) !== -1 && operators.indexOf(key) !== -1) {
-    operation = operation.replace(/.$/, key);
-    input.innerHTML = operation;
-    return;
-  }
-
-  if (key) {
-    if (key === ".") decimalAdded = true;
-    operation += key;
-    input.innerHTML = operation;
-    return;
-  }
+let numbers = document.querySelectorAll(".column .number");
+let result = document.getElementById("result");
+let operators = document.querySelectorAll(".operators .operator");
+let equalBtn = document.getElementById("equal-btn");
+let res = document.getElementsByClassName("result");
+let deleteBtn = document.getElementById("delete-btn");
+let delOneELm = document.getElementById("del-one-el");
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", (e) => {
+    result.innerHTML += e.target.innerHTML;
+    
+    if (
+      result.innerHTML.slice(0, 1) == 0 ||
+      result.innerHTML.slice(0, 1) == "."
+    ) {
+      result.innerHTML = "";
+    }
+  });
 }
 
-const evaluate = (e) => {
-  const key = e.target.dataset.key;
-  const lastChar = operation[operation.length - 1];
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", (e) => {
+    result.innerHTML += e.target.innerHTML;
 
-  if (key === "=" && operators.indexOf(lastChar) !== -1) {
-    operation = operation.slice(0, -1);
-  }
-
-  if (operation.length === 0) {
-    answer = "";
-    result.innerHTML = answer;
-    return;
-  }
-
-  try {
-    if (operation[0] === "0" && operation[1] !== "." && operation.length > 1) {
-      operation = operation.slice(1);
+    if (
+      result.innerHTML.slice(0, 1) == "+" ||
+      result.innerHTML.slice(0, 1) == "-" ||
+      result.innerHTML.slice(0, 1) == "*" ||
+      result.innerHTML.slice(0, 1) == "/"
+    ) {
+      result.innerHTML = "";
     }
-
-    const final = operation.replace(/x/g, "*").replace(/÷/g, "/");
-    answer = +eval(final).toFixed(5);
-
-    if (key === "=") {
-      decimalAdded = false;
-      operation = `${answer}`;
-      answer = "";
-      input.innerHTML = operation;
-      result.innerHTML = answer;
-      return;
-    }
-
-    result.innerHTML = answer;
-  } catch (e) {
-    if (key === "=") {
-      decimalAdded = false;
-      input.innerHTML = `<span class="error">${operation}</span>`;
-      result.innerHTML = `<span class="error">Bad Expression</span>`;
-    }
-    console.log(e);
-  }
+  });
 }
 
-const clearInput = (e) => {
-  if (e.ctrlKey) {
-    operation = "";
-    answer = "";
-    input.innerHTML = operation;
-    result.innerHTML = answer;
-    return;
+equalBtn.addEventListener("click", () => {
+  result.innerHTML = eval(result.innerHTML);
+  if (result.innerHTML.split("")[result.innerHTML.length - 1] == ".") {
+    result.innerHTML = result.innerHTML.slice(0, 1);
   }
+});
 
-  operation = operation.slice(0, -1);
-  input.innerHTML = operation;
-}
+deleteBtn.addEventListener("click", () => {
+  result.innerHTML = "";
+});
 
-deleteBtn.addEventListener("click", clearInput);
-keys.forEach((key) => {
-  key.addEventListener("click", handleKeyPress);
-  key.addEventListener("click", evaluate);
+delOneELm.addEventListener("click", () => {
+  result.innerHTML = result.innerHTML.slice(0, -1);
 });
